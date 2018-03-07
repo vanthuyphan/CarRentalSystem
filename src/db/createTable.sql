6d8f9fdedafce390a745a10b70ff46f7c4ac9757
Enkhee <3nkh33@gmail.com>
2018-03-07 15:28:29 -0600

    login changes

--- src/db/createTable.sql
+++ src/db/createTable.sql
@@ -1,16 +1,23 @@
 create database car_rental_system;
 use car_rental_system;
 CREATE TABLE IF NOT EXISTS `User` (
-	`code` BIGINT NOT NULL AUTO_INCREMENT = 1000,
-	`id` VARCHAR(100) UNIQUE,
-	`email` VARCHAR(100) UNIQUE,
-	`first_name` VARCHAR(100),
-	`last_name` VARCHAR(100),
-	`middle_name` VARCHAR(100),
+	`user_id` BIGINT NOT NULL AUTO_INCREMENT,
+	`name` VARCHAR(100),
+    `email` VARCHAR(100) UNIQUE,
 	`password` VARCHAR(20),
 	`phone` VARCHAR(20),
-	`street` VARCHAR(100),
-	`zip` VARCHAR(10),
-	PRIMARY KEY (`code`)
+	`address` VARCHAR(100),
+    `type` INT,
+    `status` INT,
+    `created` DATETIME,
+    `last_login` DATETIME,
+    `insurance_company` VARCHAR(100),
+	PRIMARY KEY (`user_id`)
+) ENGINE MyISAM DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+
+CREATE TABLE IF NOT EXISTS `UserType` (
+	`type_id` BIGINT NOT NULL AUTO_INCREMENT,
+	`type` VARCHAR(100),
+	PRIMARY KEY (`type_id`)
 ) ENGINE MyISAM DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
