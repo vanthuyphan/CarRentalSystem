f005fa6f80144effa17a2c4f983a3bcb4e2bdc73
mena <mena.emile@gmail.com>
2018-03-07 15:03:13 -0600

    database car table 2

--- src/db/createTable.sql
+++ src/db/createTable.sql
@@ -13,4 +13,19 @@ CREATE TABLE IF NOT EXISTS `User` (
 	`zip` VARCHAR(10),
 	PRIMARY KEY (`code`)
 ) ENGINE MyISAM DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+--------------------------------------------------------------------------------------
+-- (Mena) Tables
+CREATE TABLE `car` (
+  `id` bigint(20) NOT NULL,
+  `CarType` varchar(100) DEFAULT NULL,
+  `ProviderComp` varchar(100) DEFAULT NULL,
+  `Passenger` varchar(100) DEFAULT NULL,
+  `Price` varchar(100) DEFAULT NULL,
+  `Color` varchar(100) DEFAULT NULL,
+  `IsAvailable` varchar(100) DEFAULT NULL,
+  `Status` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+--------------------------------------------------------------------------------------
+
 
